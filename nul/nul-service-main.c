479a436e4516bbecf746b65dcf9e9f4077abd694
Jordan Yelloz <jordan@yelloz.me>
2014-08-23 19:04:30 -0700

    And it was a C project.

---
+++ nul/nul-service-main.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+#include <glib.h>
+#include <gio/gio.h>
+
+gint
+main (const gint          argc,
+      const gchar **const argv)
+{
+    return 0;
+}
---
+++ nul/nul-ui-main.c
@@ -0,0 +1,11 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+#include <glib.h>
+
+gint
+main (const gint          argc,
+      const gchar **const argv)
+{
+    return 0;
+}
