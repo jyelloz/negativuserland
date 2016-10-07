2c7fbae4c3cf6ed918b722814a8ff759466371e3
Jordan Yelloz <jordan@yelloz.me>
2016-10-07 13:08:21 -0700

    nul-window: removed auto-generated license block
    Also, added to autotools build and fixed wrong parent class in struct.

--- nul/nul-window.c
+++ nul/nul-window.c
@@ -1,36 +1,11 @@
-/* nul-window.c
- *
- * Copyright (C) 2016 Jordan Yelloz <jordan@yelloz.me>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "nul-window.h"
 
 struct _NulWindow
 {
-  GObject parent_instance;
+  GtkWindow parent_instance;
 };
 
 G_DEFINE_TYPE (NulWindow, nul_window, GTK_TYPE_WINDOW)
-
-enum {
-  PROP_0,
-  N_PROPS
-};
-
-/* static GParamSpec *properties [N_PROPS]; */
 
 NulWindow *
 nul_window_new (void)
@@ -45,43 +20,14 @@ nul_window_finalize (GObject *object)
 }
 
 static void
-nul_window_get_property (GObject    *object,
-                         guint       prop_id,
-                         GValue     *value,
-                         GParamSpec *pspec)
-{
-  switch (prop_id)
-    {
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-    }
-}
-
-static void
-nul_window_set_property (GObject      *object,
-                         guint         prop_id,
-                         const GValue *value,
-                         GParamSpec   *pspec)
-{
-  switch (prop_id)
-    {
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-    }
-}
-
-static void
 nul_window_class_init (NulWindowClass *klass)
 {
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
   object_class->finalize = nul_window_finalize;
-  object_class->get_property = nul_window_get_property;
-  object_class->set_property = nul_window_set_property;
 }
 
 static void
 nul_window_init (NulWindow *self)
 {
 }
-
--- nul/nul-window.h
+++ nul/nul-window.h
@@ -1,21 +1,3 @@
-/* nul-window.h
- *
- * Copyright (C) 2016 Jordan Yelloz <jordan@yelloz.me>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #ifndef NUL_WINDOW_H
 #define NUL_WINDOW_H
 
@@ -28,7 +10,8 @@ G_BEGIN_DECLS
 
 G_DECLARE_FINAL_TYPE (NulWindow, nul_window, NUL, WINDOW, GtkWindow)
 
-NulWindow *nul_window_new (void);
+NulWindow *
+nul_window_new (void);
 
 G_END_DECLS
 
