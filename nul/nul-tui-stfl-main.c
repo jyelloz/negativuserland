5699befe528694240d4d2e6c523b315e652403af
Jordan Yelloz <jordan@yelloz.me>
2016-10-08 09:42:02 -0700

    tui: added tui markup and updated tui code

--- nul/nul-tui-stfl-main.c
+++ nul/nul-tui-stfl-main.c
@@ -1,13 +1,79 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
+#include <langinfo.h>
 
 #include <stfl.h>
 
 #include <glib.h>
 #include <gio/gio.h>
 
+#include "nul-music-service.h"
+
 static struct stfl_form *form = NULL;
+static struct stfl_ipool *ipool = NULL;
+
+static void
+artists_ready_cb (NulMusicService *const proxy,
+                  GAsyncResult    *const result,
+                  gpointer         const user_data)
+{
+
+  g_autofree gchar **artists = NULL;
+
+  nul_music_service_call_get_artists_finish (proxy, &artists, result, NULL);
+
+  stfl_modify (
+    form,
+    L"artists",
+    L"replace",
+    L"list[artists]"
+  );
+
+  for (gchar *const *artist = artists; artist != NULL; artist++) {
+
+    if (*artist == NULL) {
+      break;
+    }
+
+    g_autofree gchar *line = g_strdup_printf (
+      "{listitem text:\"%s\"}",
+      *artist
+    );
+
+    g_autofree wchar_t const *line_stfl = stfl_ipool_towc (
+      ipool,
+      line
+    );
+
+    stfl_modify (
+      form,
+      L"artists",
+      L"append",
+      line_stfl
+    );
+
+  }
+
+  stfl_run (form, -1);
+
+}
+
+static void
+music_service_ready_cb (NulMusicService *const proxy,
+                        GAsyncResult    *const result,
+                        gpointer         const user_data)
+{
+  nul_music_service_call_get_artists (
+    proxy,
+    1000,
+    50,
+    NULL,
+    (GAsyncReadyCallback) artists_ready_cb,
+    NULL
+  );
+}
 
 static void
 service_appeared_cb (GDBusConnection *const conn,
@@ -16,10 +82,20 @@ service_appeared_cb (GDBusConnection *const conn,
                      gpointer         const user_data)
 {
 
-  /* g_message ("service appeared"); */
-  stfl_set (form, L"root_style", L"bg=green");
+  stfl_set (form, L"status_area_style", L"bg=blue,attr=bold");
+  stfl_set (form, L"status_label_text", L"CONNECTED");
   stfl_redraw ();
   stfl_run (form, -1);
+
+  nul_music_service_proxy_new (
+    conn,
+    G_DBUS_PROXY_FLAGS_NONE,
+    "org.negativuserland.Service",
+    "/org/negativuserland/Service",
+    NULL,
+    (GAsyncReadyCallback) music_service_ready_cb,
+    user_data
+  );
 
 }
 
@@ -29,16 +105,19 @@ service_vanished_cb (GDBusConnection *const conn,
                      gpointer         const user_data)
 {
 
-  /* g_message ("service vanished"); */
-  stfl_set (form, L"root_style", L"bg=red");
+  stfl_set (form, L"status_area_style", L"bg=yellow");
+  stfl_set (form, L"status_label_text", L"DOWN");
+  stfl_modify (
+    form,
+    L"artists",
+    L"replace",
+    L"list[artists]"
+  );
+
   stfl_redraw ();
   stfl_run (form, -1);
 
 }
-
-static const int *const markup =
-L"vbox"
-L"  @style_normal[root_style]:bg=blue";
 
 static void
 startup_cb (GApplication *const app,
@@ -47,7 +126,9 @@ startup_cb (GApplication *const app,
 
   GDBusConnection *const conn = g_application_get_dbus_connection (app);
 
-  form = stfl_create (markup);
+  form = stfl_create (L"<data/tui.stfl>");
+  ipool = stfl_ipool_create (nl_langinfo (CODESET));
+  stfl_set (form, L"title_label_text", L"negatıvuserland");
 
   g_bus_watch_name_on_connection (
     conn,
@@ -67,22 +148,42 @@ static void
 activate_cb (GApplication *const app,
              gpointer      const user_data)
 {
-  g_message ("activated");
+
+}
+
+static gboolean
+input_cb (GIOChannel   *const chan,
+          GIOCondition  const cond,
+          GApplication *const app)
+{
+
+  wchar_t const *const event = stfl_run (form, 0);
+
+  stfl_set (form, L"event_label_text", event ? event : L"");
+  stfl_run (form, -1);
+
+  return G_SOURCE_CONTINUE;
+
 }
 
 gint
-main (const gint          argc,
-      const gchar **const argv)
+main (gint const          argc,
+      gchar const **const argv)
 {
+
+  setlocale (LC_ALL, "");
 
   GApplication *const app = g_application_new (
     "org.negativuserland.Tui",
     G_APPLICATION_IS_SERVICE
   );
+  GIOChannel *const stdin_channel = g_io_channel_unix_new (STDIN_FILENO);
 
+  g_io_add_watch (stdin_channel, G_IO_IN, (GIOFunc) input_cb, app);
   g_signal_connect (app, "startup", G_CALLBACK (startup_cb), NULL);
   g_signal_connect (app, "activate", G_CALLBACK (activate_cb), NULL);
 
+  g_application_set_default (app);
   g_application_hold (app);
 
   return g_application_run (app, 0, NULL);
