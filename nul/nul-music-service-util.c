e13e23986dacef8932231e04c8c8f84578aa82ed
Jordan Yelloz <jordan@yelloz.me>
2017-01-23 20:18:00 -0800

    nul-music-service-util: added album-filtering code

--- nul/nul-music-service-util.c
+++ nul/nul-music-service-util.c
@@ -3,10 +3,29 @@
 #include "nul-music-service.h"
 
 #include <glib.h>
+#include <gee.h>
+
+#include <string.h>
 
 #include "artists.c"
 #include "albums.c"
 #include "tracks.c"
+
+static GeeCollection *
+get_tracks_by_album (NulMusicService *const self,
+                     gchar const     *const album_id)
+{
+
+  GeeMultiMap *const tracks_by_album = GEE_MULTI_MAP (
+    g_object_get_data (
+      G_OBJECT (self),
+      "tracks-by-album"
+    )
+  );
+
+  return gee_multi_map_get (tracks_by_album, album_id);
+
+}
 
 static gboolean
 handle_get_artists (NulMusicService       *const self,
@@ -137,11 +156,98 @@ handle_get_tracks (NulMusicService       *const self,
 
 }
 
+static gboolean
+handle_get_tracks_for_album (NulMusicService       *const self,
+                             GDBusMethodInvocation *const invo,
+                             gchar   const         *const album_id,
+                             guint64 const                offset,
+                             guint64 const                limit)
+{
+
+  GeeCollection *const album_tracks = get_tracks_by_album (self, album_id);
+  guint64 const n_album_tracks = GEE_IS_COLLECTION (album_tracks) ?
+    gee_collection_get_size (album_tracks) : 0;
+  gsize const slice_length = CLAMP (n_album_tracks - offset, 0, limit);
+
+  g_debug (
+    "album_id=%s"
+    ", offset=%" G_GSIZE_FORMAT
+    ", limit=%" G_GSIZE_FORMAT
+    ", slice_length=%" G_GSIZE_FORMAT
+    ", n_tracks=%" G_GUINT64_FORMAT,
+    album_id,
+    offset,
+    limit,
+    slice_length,
+    n_album_tracks
+  );
+
+  if (offset >= n_album_tracks) {
+    g_debug ("offset is over the edge, returning empty list");
+    g_dbus_method_invocation_return_value (invo, g_variant_new ("(as)", NULL));
+    g_object_unref (album_tracks);
+    return TRUE;
+  }
+
+  g_autofree gchar const **album_tracks_array = (gchar const **)
+    gee_collection_to_array (album_tracks, NULL);
+
+  g_autofree gchar const **slice_array = g_memdup (
+    &album_tracks_array[offset],
+    slice_length * sizeof (gchar const *)
+  );
+
+  GVariant *const slice = g_variant_new_strv (
+    slice_array,
+    slice_length
+  );
+
+  g_dbus_method_invocation_return_value (
+    invo,
+    g_variant_new_tuple (&slice, 1)
+  );
+
+  g_object_unref (album_tracks);
+
+  return TRUE;
+
+}
+
+static GeeMultiMap *
+build_index_tracks_by_album (void)
+{
+
+  GeeTreeMultiMap *const tracks_by_album = gee_tree_multi_map_new (
+    G_TYPE_STRING,
+    (GBoxedCopyFunc) g_strdup,
+    g_free,
+    G_TYPE_STRING,
+    (GBoxedCopyFunc) g_strdup,
+    g_free,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL
+  );
+
+  return GEE_MULTI_MAP (tracks_by_album);
+
+}
+
 GDBusInterfaceSkeleton *
 nul_music_service_util_get_skeleton (void)
 {
 
   NulMusicService *const music = nul_music_service_skeleton_new ();
+
+  g_object_set_data_full (
+    G_OBJECT (music),
+    "tracks-by-album",
+    build_index_tracks_by_album (),
+    (GDestroyNotify) g_object_unref
+  );
 
   g_signal_connect (
     music,
@@ -161,6 +267,13 @@ nul_music_service_util_get_skeleton (void)
     music,
     "handle-get-tracks",
     G_CALLBACK (handle_get_tracks),
+    NULL
+  );
+
+  g_signal_connect (
+    music,
+    "handle-get-tracks-for-album",
+    G_CALLBACK (handle_get_tracks_for_album),
     NULL
   );
 
