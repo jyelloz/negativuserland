c23fde1924c96b4aa7c7e0360c28c5736b3a36f0
Jordan Yelloz <jordan@yelloz.me>
2017-02-11 23:22:56 -0800

    nul-external-autocleanups: made declarations conditional
    This requires that the files are included by the module before this include, though.

--- nul/nul-external-autocleanups.h
+++ nul/nul-external-autocleanups.h
@@ -2,15 +2,18 @@
 #define NUL_EXTERNAL_AUTOCLEANUPS_H
 
 #include <glib.h>
-#include <tracker-sparql.h>
-#include <gee.h>
+#include <glib-object.h>
 
+#ifdef __GEE_H__
 G_DEFINE_AUTOPTR_CLEANUP_FUNC (GeeList, g_object_unref)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC (GeeCollection, g_object_unref)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC (GeeMultiMap, g_object_unref)
+#endif
 
+#ifdef __LIBTRACKER_SPARQL_H__
 G_DEFINE_AUTOPTR_CLEANUP_FUNC (TrackerSparqlConnection, g_object_unref)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC (TrackerSparqlBuilder, g_object_unref)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC (TrackerSparqlCursor, g_object_unref)
+#endif
 
 #endif /* NUL_EXTERNAL_AUTOCLEANUPS_H */
