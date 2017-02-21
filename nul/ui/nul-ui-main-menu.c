50a4153ca174b462bc96e83c049f66a3ae3926dc
Jordan Yelloz <jordan@yelloz.me>
2017-02-20 22:56:11 -0800

    nul-ui-main-menu: added basic GActions for artists/albums browsing.

--- nul/ui/nul-ui-main-menu.c
+++ nul/ui/nul-ui-main-menu.c
@@ -1,5 +1,10 @@
 #include "nul-ui-main-menu.h"
 
+#include "nul-ui-application.h"
+
+#include "nul-ui-artists.h"
+#include "nul-ui-artist-albums.h"
+
 #include <gio/gio.h>
 #include <gtk/gtk.h>
 
@@ -13,6 +18,9 @@ struct _NulUiMainMenu {
   GtkWidget *settings;
 
   GtkStack *connected_state;
+
+  NulUiArtists *artists;
+  NulUiArtistAlbums *artist_albums;
 
 };
 
@@ -38,6 +46,139 @@ activate_music (GSimpleAction *const action,
     self->connected_state,
     self->music_screen
   );
+}
+
+static inline GtkStack *
+get_music_screen_stack (NulUiApplication *const app)
+{
+
+  GtkBuilder *const bld = nul_ui_application_get_builder (app);
+
+  return GTK_STACK (gtk_builder_get_object (bld, "music-screen-stack"));
+
+}
+
+static void
+artists_ready_cb (NulMusicService *const music,
+                  GAsyncResult    *const result,
+                  NulUiMainMenu   *const self)
+{
+
+  nul_debug ("artists are ready");
+
+  NulUiApplication *const app = nul_ui_application_get_default ();
+  GtkStack *const stack = get_music_screen_stack (app);
+
+  g_autoptr(GError) error = NULL;
+  g_autoptr(GVariant) artists = NULL;
+
+  nul_music_service_call_get_artists_finish (
+    music,
+    &artists,
+    result,
+    &error
+    );
+
+  if (error) {
+    nul_error ("failed to get artists: %s", error->message);
+  }
+
+  nul_ui_artists_update (self->artists, artists);
+
+  gtk_stack_set_visible_child_name (
+    stack,
+    "artists-list-box"
+  );
+
+}
+
+static void
+activate_music_artists (GSimpleAction *const action,
+                        GVariant      *const parameter,
+                        gpointer       const user_data)
+{
+
+  nul_debug ("activating artists");
+
+  NulUiApplication *const app = nul_ui_application_get_default ();
+  NulMusicService *const music = nul_ui_application_get_music (app);
+
+  nul_music_service_call_get_artists (
+    music,
+    0,
+    10,
+    NULL,
+    (GAsyncReadyCallback) artists_ready_cb,
+    user_data
+  );
+
+}
+
+static void
+artist_albums_ready_cb (NulMusicService *const music,
+                        GAsyncResult    *const result,
+                        NulUiMainMenu   *const self)
+{
+
+  nul_debug ("artist albums are ready");
+
+  NulUiApplication *const app = nul_ui_application_get_default ();
+  GtkStack *const stack = get_music_screen_stack (app);
+
+  g_autoptr(GError) error = NULL;
+  g_autoptr(GVariant) albums = NULL;
+
+  nul_music_service_call_get_albums_for_artist_finish (
+    music,
+    &albums,
+    result,
+    &error
+  );
+
+  if (error) {
+    nul_error ("failed to get albums for artist: %s", error->message);
+  }
+
+  nul_ui_artist_albums_update (self->artist_albums, albums);
+
+  gtk_stack_set_visible_child_name (
+    GTK_STACK (stack),
+    "artist-albums-list-box"
+  );
+
+}
+
+static void
+activate_music_artist_albums (GSimpleAction *const action,
+                              GVariant      *const parameter,
+                              gpointer       const user_data)
+{
+
+  guint64 const artist_id = g_variant_get_uint64 (parameter);
+  nul_debug ("activating albums for artist#%" G_GUINT64_FORMAT, artist_id);
+
+  NulUiApplication *const app = nul_ui_application_get_default ();
+  NulMusicService *const music = nul_ui_application_get_music (app);
+
+  nul_music_service_call_get_albums_for_artist (
+    music,
+    artist_id,
+    0,
+    10,
+    NULL,
+    (GAsyncReadyCallback) artist_albums_ready_cb,
+    user_data
+  );
+
+}
+
+static void
+activate_music_album_tracks (GSimpleAction *const action,
+                             GVariant      *const parameter,
+                             gpointer       const user_data)
+{
+  guint64 const album_id = g_variant_get_uint64 (parameter);
+  nul_debug ("activating album#%" G_GUINT64_FORMAT, album_id);
 }
 
 static void
@@ -79,18 +220,23 @@ activate_settings (GSimpleAction *const action,
 static GActionEntry const entries[] = {
   {"main-menu", activate_main_menu, NULL, NULL, NULL},
   {"music", activate_music, NULL, NULL, NULL},
+  {"music-artists", activate_music_artists, NULL, NULL, NULL},
+  {"music-artist-albums", activate_music_artist_albums, "t", NULL, NULL},
+  {"music-album-tracks", activate_music_album_tracks, "t", NULL, NULL},
   {"geolocation", activate_geolocation, NULL, NULL, NULL},
   {"automotive", activate_automotive, NULL, NULL, NULL},
   {"settings", activate_settings, NULL, NULL, NULL},
 };
 
 NulUiMainMenu *
-nul_ui_main_menu_new (GtkListBox *const main_menu,
-                      GtkWidget  *const music_screen,
-                      GtkWidget  *const geolocation,
-                      GtkWidget  *const automotive,
-                      GtkWidget  *const settings,
-                      GtkStack   *const connected_state)
+nul_ui_main_menu_new (GtkListBox        *const main_menu,
+                      GtkWidget         *const music_screen,
+                      GtkWidget         *const geolocation,
+                      GtkWidget         *const automotive,
+                      GtkWidget         *const settings,
+                      GtkStack          *const connected_state,
+                      NulUiArtists      *const artists,
+                      NulUiArtistAlbums *const artist_albums)
 {
 
   NulUiMainMenu *const self = g_new0 (NulUiMainMenu, 1);
@@ -101,6 +247,8 @@ nul_ui_main_menu_new (GtkListBox *const main_menu,
   self->automotive = g_object_ref (automotive);
   self->settings = g_object_ref (settings);
   self->main_menu = g_object_ref (main_menu);
+  self->artists = g_object_ref (artists);
+  self->artist_albums = g_object_ref (artist_albums);
 
   return self;
 
@@ -128,6 +276,8 @@ nul_ui_main_menu_free (NulUiMainMenu *const self)
   g_clear_object (&self->automotive);
   g_clear_object (&self->settings);
   g_clear_object (&self->main_menu);
+  g_clear_object (&self->artists);
+  g_clear_object (&self->artist_albums);
 
   g_free (self);
 }
--- nul/ui/nul-ui-main-menu.h
+++ nul/ui/nul-ui-main-menu.h
@@ -5,25 +5,30 @@
 #include <gio/gio.h>
 #include <gtk/gtk.h>
 
+#include "nul-ui-artists.h"
+#include "nul-ui-artist-albums.h"
+
 G_BEGIN_DECLS
 
 typedef struct _NulUiMainMenu NulUiMainMenu;
 
 NulUiMainMenu *
-nul_ui_main_menu_new (GtkListBox *const main_menu,
-                      GtkWidget  *const music_screen,
-                      GtkWidget  *const geolocation,
-                      GtkWidget  *const automotive,
-                      GtkWidget  *const settings,
-                      GtkStack   *const connected_state);
+nul_ui_main_menu_new              (GtkListBox        *const main_menu,
+                                   GtkWidget         *const music_screen,
+                                   GtkWidget         *const geolocation,
+                                   GtkWidget         *const automotive,
+                                   GtkWidget         *const settings,
+                                   GtkStack          *const connected_state,
+                                   NulUiArtists      *const artists,
+                                   NulUiArtistAlbums *const artist_albums);
 
 void
-nul_ui_main_menu_free (NulUiMainMenu *const self);
+nul_ui_main_menu_free             (NulUiMainMenu     *const self);
 
 void
-nul_ui_main_menu_register_actions (NulUiMainMenu *const self,
-                                   GActionMap    *const map,
-                                   GActionGroup  *const group);
+nul_ui_main_menu_register_actions (NulUiMainMenu     *const self,
+                                   GActionMap        *const map,
+                                   GActionGroup      *const group);
 
 G_END_DECLS
 
