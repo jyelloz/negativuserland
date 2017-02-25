5246dd33ad5279840e0d5e34431abee4d90f1bee
Jordan Yelloz <jordan@yelloz.me>
2017-02-24 21:28:33 -0800

    page-browser: added vtable for handling select action

--- nul/ui/nul-ui-page-browser.h
+++ nul/ui/nul-ui-page-browser.h
@@ -7,9 +7,17 @@ G_BEGIN_DECLS
 
 #define NUL_UI_TYPE_PAGE_BROWSER (nul_ui_page_browser_get_type ())
 
+typedef struct _NulUiPageBrowser NulUiPageBrowser;
+
 struct _NulUiPageBrowserClass
 {
+
   GtkBoxClass parent_class;
+
+  void (* handle_select) (NulUiPageBrowser *self,
+                          GtkTreeModel     *model,
+                          GtkTreeIter      *iter);
+
 };
 
 G_DECLARE_DERIVABLE_TYPE (
@@ -19,6 +27,9 @@ G_DECLARE_DERIVABLE_TYPE (
   PAGE_BROWSER,
   GtkBox
 )
+
+NulUiPageBrowser *
+nul_ui_page_browser_new (void);
 
 G_END_DECLS
 
