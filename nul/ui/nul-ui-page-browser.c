8fc67be13e0a22cc870058730f4f9a37552b4c0b
Jordan Yelloz <jordan@yelloz.me>
2017-02-27 15:37:43 -0800

    nul-ui-page-browser: converted to GtkFlowBox

--- nul/ui/nul-ui-page-browser.c
+++ nul/ui/nul-ui-page-browser.c
@@ -1,12 +1,20 @@
 #include "nul-ui-page-browser.h"
 
-#define get_priv nul_ui_page_browser_get_instance_private
+#include "nul-external-autocleanups.h"
+
+#define get_priv(self) \
+  nul_ui_page_browser_get_instance_private (NUL_UI_PAGE_BROWSER (self))
 
 typedef struct
 {
 
   GtkButton *prev_button;
   GtkButton *next_button;
+  GtkLabel *status_label;
+
+  GtkFlowBox *flow;
+  GListStore *model;
+
 
 } NulUiPageBrowserPrivate;
 
@@ -14,7 +22,31 @@ G_DEFINE_ABSTRACT_TYPE (
   NulUiPageBrowser,
   nul_ui_page_browser,
   GTK_TYPE_BOX
-)
+);
+
+static void
+child_activated_cb (GtkFlowBox       *const flow,
+                    GtkFlowBoxChild  *const child,
+                    NulUiPageBrowser *const self)
+{
+
+  NulUiPageBrowserPrivate *const priv = get_priv (self);
+  GListModel *const model = G_LIST_MODEL (priv->model);
+  gint const index = gtk_flow_box_child_get_index (child);
+
+  GObject *const item = g_list_model_get_item (model, index);
+
+  gchar const *const text = g_object_get_data (item, "text");
+
+  nul_debug (
+    "row %d activated, text=%s user_data is %s@%p",
+    index,
+    text,
+    G_OBJECT_TYPE_NAME (self),
+    self
+  );
+
+}
 
 static void
 nul_ui_page_browser_class_init (NulUiPageBrowserClass *const cls)
@@ -39,12 +71,84 @@ nul_ui_page_browser_class_init (NulUiPageBrowserClass *const cls)
     next_button
   );
 
+  gtk_widget_class_bind_template_child_private (
+    widget_class,
+    NulUiPageBrowser,
+    status_label
+  );
+
+  gtk_widget_class_bind_template_child_private (
+    widget_class,
+    NulUiPageBrowser,
+    flow
+  );
+
+  gtk_widget_class_bind_template_callback (
+    widget_class,
+    child_activated_cb
+  );
+
+}
+
+static inline void
+append (GListStore  *const model,
+        gchar const *const text)
+{
+  GObject *const obj = g_object_new (G_TYPE_OBJECT, NULL);
+  g_object_set_data (obj, "text", text);
+  g_list_store_append (model, obj);
+}
+
+static GtkWidget *
+create_widget (GObject          *const item,
+               NulUiPageBrowser *const self)
+{
+  return gtk_label_new (g_object_get_data (item, "text"));
 }
 
 static void
 nul_ui_page_browser_init (NulUiPageBrowser *const self)
 {
 
+  NulUiPageBrowserPrivate *const priv = get_priv (self);
+
+  GListStore *const model = priv->model = g_list_store_new (G_TYPE_OBJECT);
+
   gtk_widget_init_template (GTK_WIDGET (self));
 
+  gtk_label_set_text (
+    priv->status_label,
+    g_strdup_printf (
+      "%s#%p",
+      G_OBJECT_TYPE_NAME (self),
+      self
+    )
+  );
+
+  g_signal_connect (
+    self,
+    "map",
+    G_CALLBACK (map_cb),
+    NULL
+  );
+
+  gtk_flow_box_bind_model (
+    priv->flow,
+    G_LIST_MODEL (model),
+    (GtkFlowBoxCreateWidgetFunc) create_widget,
+    self,
+    NULL
+  );
+
+}
+
+NulUiPageBrowser *
+nul_ui_page_browser_new (void)
+{
+  return g_object_new (
+    NUL_UI_TYPE_PAGE_BROWSER,
+    "width-request", 720,
+    "height-request", 480,
+    NULL
+  );
 }
